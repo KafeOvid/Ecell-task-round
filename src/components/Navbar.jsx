231a9a6c77091a7532919e9887e25670ed16a1a2
KafeOvid <ayushmanng04@gmail.com>
2024-10-26 17:05:04 +0530

    Fix Navbar Mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // Import useState
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; // Assuming you're using React Router
 import '../styles/Navbar.css';
 import logo from '../assets/logo.png'; // Adjust the path to your logo image
@@ -14,25 +14,25 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <img src={logo} alt="Ecell Logo" className="logo" />
+        <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
+          <li>
+            <Link to="#about">About</Link>
+          </li>
+          <li>
+            <Link to="#data">Data</Link>
+          </li>
+          <li>
+            <Link to="#sponsors">Sponsors</Link>
+          </li>
+          <li>
+            <Link to="#contact">Contact</Link>
+          </li>
+        </ul>
+        <button className="join-button">JOIN NOW</button>
         <button className="toggle-button" onClick={toggleMenu}>
           &#9776; {/* Hamburger icon */}
         </button>
       </div>
-      <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
-        <li>
-          <Link to="#about">About</Link>
-        </li>
-        <li>
-          <Link to="#data">Data</Link>
-        </li>
-        <li>
-          <Link to="#sponsors">Sponsors</Link>
-        </li>
-        <li>
-          <Link to="#contact">Contact</Link>
-        </li>
-      </ul>
-      <button className="join-button">JOIN NOW</button>
     </nav>
   );
 };
